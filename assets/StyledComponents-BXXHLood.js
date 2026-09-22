093b0e16038177b80a5375dba70f3ccbd43d8e70
naser gis <mhamd.naser.edden@gmail.com>
2026-09-22 13:44:23 +0300

    test

---
+++ assets/StyledComponents-BXXHLood.js
@@ -0,0 +1,47 @@
+import{dD as o,dE as a}from"./vendor-CIeFKq1C.js";o(a)`
+  .laPPHD {
+    background-color: transparent !important;
+  }
+  .hDHZZh {
+    background-color: transparent !important;
+  }
+  margin: 0 0;
+  & .rdt_Table {
+    min-width: 100%;
+    background-color: ${r=>r.$mode==="dark"?"rgb(50, 50, 60)":"#fff"};
+    color: var(--primary-text);
+    padding: 16px;
+
+    .rdt_TableRow,
+    .rdt_TableHead,
+    .rdt_TableHeadRow {
+      background-color: var(--blocks-color);
+      color: var(--primary-text);
+    }
+    .rdt_TableRow {
+      
+    
+    }
+    .rdt_TableHeadRow {
+      font-size: 16px;
+    }
+    .jWOtlo:not(:last-of-type),
+    .idxdtx {
+      border-bottom-color: ${r=>r.$mode==="dark"?"rgb(80, 80, 90)":"rgb(200, 200, 200)"};
+    }
+  }
+  ~ div .rdt_Pagination {
+    background-color: ${r=>r.$mode==="dark"?"rgb(50, 50, 60)":"#fff"};
+    color: var(--primary-text);
+    padding: 16px;
+
+    .dihybH {
+      color: var(--primary-text);
+      &:disabled {
+        color: rgb(190, 200, 210) !important;
+      }
+    }
+  }
+`;const d=o.div`
+  margin: 2rem 0px;
+`;export{d as P};
